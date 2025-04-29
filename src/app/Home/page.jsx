740e90971f8d83390b78765f515575370624b166
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2025-04-29 18:08:29 +0500

    fixing responsivenees

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -18,7 +18,7 @@ export default function HeroSection() {
         <Navbar />
 
         {/* Hero Text */}
-        <div className="flex items-center h-full px-6 md:px-16 pt-20 md:pt-32 relative z-10">
+        <div className="flex items-center h-full px-4 sm:px-6 md:px-16 pt-20 md:pt-32 relative z-10">
           <div className="max-w-2xl text-white space-y-6">
             <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold leading-tight">
               Welcome To <span className="text-red-600">The</span>
@@ -33,7 +33,7 @@ export default function HeroSection() {
       </div>
 
       {/* Sections */}
-      <div className="px-4 md:px-8 lg:px-16 space-y-12 mt-12">
+      <div className="px-4 sm:px-6 md:px-10 lg:px-16 space-y-12 mt-12">
         <Categorysection />
         <Feautredcard />
         <Menscard />
